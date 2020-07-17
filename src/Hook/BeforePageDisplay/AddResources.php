71efa04ffd0ab4b74789d2a88af6df27aa5c2f2f
dvogel <daniel.vogel@hallowelt.com>
2020-07-17 08:14:30 +0200

    Chapter pager before and after content and in navigation-main
    [3.1.x]
    
    ERM19554
    
    Change-Id: I80357698c4e3103fd1c5803bd1cc12f9ae91f061

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -2,12 +2,27 @@
 
 namespace BlueSpice\BookshelfUI\Hook\BeforePageDisplay;
 
+use BlueSpice\Services;
+
 class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
 		$this->out->addModuleStyles( 'ext.bluespice.bookshelfUI.styles' );
 		$this->out->addModuleStyles( 'ext.bluespice.bookshelfUI.navigationTab.styles' );
 		$this->out->addModules( 'ext.bluespice.bookshelfUI.navigationTab' );
+		$this->out->addModuleStyles( 'ext.bluespice.bookshelfUI.pager.navigation.styles' );
+
+		$config = Services::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		$pagerBeforeContent = $config->get( 'BookShelfUIShowChapterNavigationPagerBeforeContent' );
+		$pagerAfterContent = $config->get( 'BookShelfUIShowChapterNavigationPagerAfterContent' );
+
+		if ( ( $pagerBeforeContent === true ) || ( $pagerBeforeContent === 1 ) ) {
+				$this->out->addModuleStyles( 'ext.bluespice.bookshelfUI.pager.before-content.styles' );
+		}
+
+		if ( ( $pagerAfterContent === true ) || ( $pagerAfterContent === 1 ) ) {
+				$this->out->addModuleStyles( 'ext.bluespice.bookshelfUI.pager.after-content.styles' );
+		}
 	}
 
 }
